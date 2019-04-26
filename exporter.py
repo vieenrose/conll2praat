68361013de919d81c8a5d54ba02770d693cbfee3
vieenrose <luigi.liu@laposte.net>
2019-04-26 20:08:43 +0200

    bugfix for short/long text TextGrid reading: 1. rename the function Tier() gloablly ie. by pympi.Praat.Tier() 2. add automatic encoding detection for TextGrid file 3. show textgrid file encoding 4. some minor esthetic arrangement (the main routine insie the__main__ subroutine)

--- exporter.py
+++ exporter.py
@@ -2,26 +2,38 @@
 # -*- coding: utf-8 -*-
 
 # exportateur Column @ CoNLL-U -> Tier @ Praat TextGrid
-# prerequisite : pympi.Praat, javaobj
+# prerequisite : pympi.Praat, javaobj, python-magic
 #
-# note: 1. javaobj could have some problem with python3 but works correctly under python2
-#
-# auteurs : 
+# auteurs :
 #     Sandy Duchemin
 #     Luigi Liu
 
-import re, csv, os, sys, argparse, collections
-from difflib import SequenceMatcher
-import pympi.Praat as prt
-from collections import deque
-import sys, codecs, re, struct, javaobj
+import csv,os,argparse,collections,sys,codecs,re,struct,difflib,pympi.Praat
+import javaobj # it has some issues with python3
+import magic
 DEBUG = False
 
 
 # outils
 
+# detectot of file coding
+# ref: https://stackoverflow.com/questions/436220/how-to-determine-the-encoding-of-text
+def get_encoding(filepath):
+	encoding = None
+	try:
+		blob = open(filepath).read()
+		m = magic.open(magic.MAGIC_MIME_ENCODING)
+		m.load()
+		encoding = m.buffer(blob)  # "utf-8" "us-ascii" etc
+	except:
+		blob = open(filepath).read()
+		m = magic.Magic(mime_encoding=True)
+		encoding = m.from_buffer(blob)
+	if u'ascii' in encoding  : encoding= u'ascii'
+	return encoding
+
 # extend original TextGrid reader to support praat Collection / Analor .or
-class TextGridPlus(prt.TextGrid):
+class TextGridPlus(pympi.Praat.TextGrid):
       
       
       def extractTextGridFromAnalorFile(self,ifile):
@@ -70,7 +82,7 @@ class TextGridPlus(prt.TextGrid):
                                       raise Exception('Tiertype does not exist.')
                         
                                 # form a tier
-                                tier = prt.Tier(0, 0, name=nom, tier_type=tier_type) # bug
+                                tier = pymip.Praat.Tier(0, 0, name=nom, tier_type=tier_type) 
                                 self.tiers.append(tier)
                                 tier.xmin = tlims[0]
                                 tier.xmax = tlims[-1]
@@ -100,8 +112,7 @@ class TextGridPlus(prt.TextGrid):
                   ignored for binary TextGrids.
               """
               # extract TextGrid form Analor file (.or)
-              
-              if self.extractTextGridFromAnalorFile(ifile): 
+              if self.extractTextGridFromAnalorFile(ifile) : 
                     pass
                     
               # read a Textgrid or extract TextGrid from Collection in Binary Format
@@ -134,7 +145,7 @@ class TextGridPlus(prt.TextGrid):
                   for i in range(self.tier_num):
                       tier_type = ifile.read(ord(ifile.read(1))).decode('ascii')
                       name = bin2str(ifile)
-                      tier = prt.Tier(0, 0, name=name, tier_type=tier_type)
+                      tier = pympi.Praat.Tier(0, 0, name=name, tier_type=tier_type)
                       self.tiers.append(tier)
                       tier.xmin = struct.unpack('>d', ifile.read(8))[0]
                       tier.xmax = struct.unpack('>d', ifile.read(8))[0]
@@ -169,11 +180,13 @@ class TextGridPlus(prt.TextGrid):
                   self.tier_num = int(nn(ifile, regint))
                   not short and next(ifile)
                   for i in range(self.tier_num):
-                      not short and next(ifile)  # skip item[]: and item[\d]:
+                      not short and next(ifile) # skip item[]: and item[\d]:
                       tier_type = nn(ifile, regstr)
                       name = nn(ifile, regstr)
-                      tier = Tier(0, 0, name=name, tier_type=tier_type)
+                      #print(name) #debug
+                      tier = pympi.Praat.Tier(0, 0, name=name, tier_type=tier_type)
                       self.tiers.append(tier)
+                      
                       tier.xmin = float(nn(ifile, regfloat))
                       tier.xmax = float(nn(ifile, regfloat))
                       for i in range(int(nn(ifile, regint))):
@@ -305,7 +318,7 @@ def one_to_many_pairing (file1, files2, thld = 5):
       
       for file2 in files2 :
             nonenone,nonenone,match_len = \
-                SequenceMatcher(None, file1.lower(), file2.lower()).\
+                difflib.SequenceMatcher(None, file1.lower(), file2.lower()).\
 		    find_longest_match(0, len(file1), 0, len(file2))
             if match_len > max(thld , maxlen):
                   maxlen = match_len
@@ -330,141 +343,161 @@ def make_paires(files1, files2):
 
       return pairs
       
-# filelists / tiernames / constants
+def show_list_of_file_pair (conll_tg_pairs, err_cnt = None, enc_dict = None, reverse = False):
+      if reverse: 
+            conll_tg_pairs = conll_tg_pairs[::-1]
+      for n,p in enumerate(conll_tg_pairs):
+            conll, tg = p
+            string_to_display = u'{}:\t{:5s}: {}\n\t{:5s}: {}'.format(n,'CoNLL-U',conll,'TextGrid',tg)
+            # encoding
+            if enc_dict : 
+                  if conll in enc_dict.keys():
+                        enc = enc_dict[tg]
+                        if enc: 
+                              string_to_display += ' [{}]\n'.format(enc)
+            # error count
+            if err_cnt:
+                  if conll in err_cnt.keys():
+                        num_err = err_cnt[conll] 
+                        if num_err: 
+                              string_to_display += '\n\tnumber of errors: {}\n'.format(num_err)
+            string_to_display += '\n'
+            print(string_to_display)
 
-# creattion of a frendly command-line interface using argparse
-parser = argparse.ArgumentParser(description='conll2praat exporter')
-parser.add_argument('conll_in', help = 'folder of conll files')
-parser.add_argument('praat_in', help = 'folder of input praat files')
-parser.add_argument('praat_out', help = 'folder for output praat files')
-args = parser.parse_args()
-# make conll - praat pairs
-conllFiles   = os.listdir(args.conll_in)
-conllFiles.sort()
-inputTgFiles = os.listdir(args.praat_in)
-conll_tg_pairs = make_paires(conllFiles, inputTgFiles)
-print("File(s) to process : ")
-for n,p in enumerate(conll_tg_pairs):
-      print('{}:\t{:5s}: {}\n\t{:5s}: {}'.format(n,'conll',p[0],'tg',p[1]))
-print('')
-conll_tg_pairs_bak = conll_tg_pairs
-out_rep     = args.praat_out
-if not os.path.exists(out_rep):
-        os.makedirs(out_rep)
-refTierNames = ['mot','MOT','TokensAlign']
-destTierName = 'tx_new'
-pauseSign    = '#'
-srcCol       = 2 # 'FORM' (CoNLL)
 
-# todo : recherche automatique du time reference tier
-#for n,p in enumerate(conll_tg_pairs_bak): print('{}:\t{:5s}: {}\n\t{:5s}: {}\n'.format(n,'conll',p[0],'tg',p[1]))
-
-# I/O handlers
-err = collections.Counter()
-conll_tg_pairs = conll_tg_pairs[::-1]
-while conll_tg_pairs:
-    inconllFile,inTgfile = conll_tg_pairs.pop()
-    #print("\tFichier CONLL traité : {}".format(inconllFile))
-    #print("\tFichier TG traité : {}\n".format(inTgfile))
-    conll_path = args.conll_in+'/'+inconllFile
-    inTg_path = args.praat_in+'/'+inTgfile
-    
-   
-    conll  = csv.reader(open(conll_path, 'r'), delimiter='\t', quotechar='\\') #lecture du fichier tabulaire (CoNLL-U)
-    try:
-      tg     = TextGridPlus(file_path=inTg_path, codec='utf-8')               #lecture du fichier textgrid (Praat)
-      outputTg_path = args.praat_out+'/'+insert_to_basename(inTgfile,'_UPDATED','TextGrid')
-    except:
-      print('error: fail to read {}'.format(inTg_path))
-      continue
+if __name__ == '__main__':
       
-    print('\t{:s} {:s}'.format('<-',conll_path))
-    print('\t{:s} {:s}'.format('<-',inTg_path))
-    print('\t{:s} {:s}'.format('->',outputTg_path))
-    
-    
-    # handel diff. reference tier names
-    for refTierName in refTierNames :
-        try:
-            ref    = tg.get_tier(refTierName)  #tier de repères temporels ('mot')
-            break
-        except IndexError:
-            pass
-            
-    dest   = tg.add_tier(destTierName) #tier de destination ('tx')
+      # filelists / tiernames / constants
 
-    # initialization
-    tokens = []
-    sentId = 0
-    pauseId = 0
-    cursor = 0
+      # creattion of a frendly command-line interface using argparse
+      parser = argparse.ArgumentParser(description='conll2praat exporter')
+      parser.add_argument('conll_in', help = 'folder of conll files')
+      parser.add_argument('praat_in', help = 'folder of input praat files')
+      parser.add_argument('praat_out', help = 'folder for output praat files')
+      args = parser.parse_args()
+      # make conll - praat pairs
+      conllFiles   = os.listdir(args.conll_in)
+      conllFiles.sort()
+      inputTgFiles = os.listdir(args.praat_in)
+      conll_tg_pairs = make_paires(conllFiles, inputTgFiles)
+      conll_tg_pairs_bak = conll_tg_pairs
+      print("File(s) to process : ")
+      show_list_of_file_pair(conll_tg_pairs_bak,reverse = False)
+      out_rep     = args.praat_out
+      if not os.path.exists(out_rep):
+              os.makedirs(out_rep)
+      refTierNames = ['mot','MOT','TokensAlign']
+      destTierName = 'tx_new'
+      pauseSign    = '#'
+      srcCol       = 2 # 'FORM' (CoNLL)
 
-    # boucle de lecture
-    for n, row in enumerate(conll) :
+      # todo : recherche automatique du time reference tier
+      #for n,p in enumerate(conll_tg_pairs_bak): print('{}:\t{:5s}: {}\n\t{:5s}: {}\n'.format(n,'conll',p[0],'tg',p[1]))
 
-    # les métadonnées 
-        if row and len(row) < 10 : 
-            metadata = row[0]
-            #deb_print("L{} mdata[:50] '{}'".format(n,metadata[:50]))
+      # I/O handlers
+      err = collections.Counter()
+      enc = collections.defaultdict()
+      conll_tg_pairs = conll_tg_pairs[::-1]
+      while conll_tg_pairs:
+          inconllFile,inTgfile = conll_tg_pairs.pop()
+          #print("\tFichier CONLL traité : {}".format(inconllFile))
+          #print("\tFichier TG traité : {}\n".format(inTgfile))
+          conll_path = args.conll_in+'/'+inconllFile
+          inTg_path = args.praat_in+'/'+inTgfile
+          
+         
+          conll  = csv.reader(open(conll_path, 'r'), delimiter='\t', quotechar='\\') #lecture du fichier tabulaire (CoNLL-U)
+          try:
+            enc[inTg_path] = get_encoding(inTg_path)
+            tg     = TextGridPlus(file_path=inTg_path, codec=enc[inTg_path])               #lecture du fichier textgrid (Praat)
+            outputTg_path = args.praat_out+'/'+insert_to_basename(inTgfile,'_UPDATED','TextGrid')
+          except:
+            print('error: fail to read {}'.format(inTg_path))
             continue
-    
-    # token dans une phrase
-        if row : 
-            token = row[srcCol - 1]
-            # récolte des tokens 
-            if token.strip() != pauseSign : 
-                tokens.append(token)
-                #deb_print("L{} tokens[-5:-1] '{}'".format(n,tokens[-5:-1]))
-            else :
-                #deb_print("L{} pause no.{} '{}'".format(n,pauseId, token))
-                pauseId += 1
-                
-    # saute de ligne à la frontière des phrases
-        else :
-            sent = ' '.join(tokens)
-            deb_print("L{} sentence no.{} '{}'".format(n,sentId,sent))
-            
-            # try a local search from cursor to end of time with by default thld.
-            [begin,end, cursor_out] = findTimes(tokens,ref, lowerbound=cursor, upperbound=cursor+50,thld = 0.10)
-            if cursor_out >= cursor : 
-                  cursor = cursor_out
-                  deb_print("L{} local (begin,end) = ({:8.3f},{:8.3f})".format(n,begin,end))
-                
-                  # écrire le contenu dans le tier de destination
-                  try:
-                        dest.add_interval(begin=begin, end=end, value=sent, check=True)
-                  except:
-                        print('Warning: Anti-overlaps')
-            else:
-                  # try a global search but with a more strict threshold for distance
-                  [begin,end, cursor_out] = findTimes(tokens,ref, lowerbound=0, upperbound=-1, thld = 0.05)
-                  if cursor_out >= 0 : 
-                        #deb_print("L{} global (begin,end) = ({:8.3f},{:8.3f})".format(n,begin,end))
+      
+          print('\t{:s} {:s}'.format('<-',conll_path))
+          print('\t{:s} {:s} [{}]'.format('<-',inTg_path, enc[inTg_path] if enc[inTg_path] else 'unknown'))
+          print('\t{:s} {:s} [{}]'.format('->',outputTg_path, 'binary'))
+          
+          
+          # handel diff. reference tier names
+          for refTierName in refTierNames :
+              try:
+                  ref    = tg.get_tier(refTierName)  #tier de repères temporels ('mot')
+                  break
+              except IndexError:
+                  pass
                   
+          dest   = tg.add_tier(destTierName) #tier de destination ('tx')
+
+          # initialization
+          tokens = []
+          sentId = 0
+          pauseId = 0
+          cursor = 0
+
+          # boucle de lecture
+          for n, row in enumerate(conll) :
+
+          # les métadonnées 
+              if row and len(row) < 10 : 
+                  metadata = row[0]
+                  #deb_print("L{} mdata[:50] '{}'".format(n,metadata[:50]))
+                  continue
+          
+          # token dans une phrase
+              if row : 
+                  token = row[srcCol - 1]
+                  # récolte des tokens 
+                  if token.strip() != pauseSign : 
+                      tokens.append(token)
+                      #deb_print("L{} tokens[-5:-1] '{}'".format(n,tokens[-5:-1]))
+                  else :
+                      #deb_print("L{} pause no.{} '{}'".format(n,pauseId, token))
+                      pauseId += 1
+                      
+          # saute de ligne à la frontière des phrases
+              else :
+                  sent = ' '.join(tokens)
+                  deb_print("L{} sentence no.{} '{}'".format(n,sentId,sent))
+                  
+                  # try a local search from cursor to end of time with by default thld.
+                  [begin,end, cursor_out] = findTimes(tokens,ref, lowerbound=cursor, upperbound=cursor+50,thld = 0.10)
+                  if cursor_out >= cursor : 
+                        cursor = cursor_out
+                        deb_print("L{} local (begin,end) = ({:8.3f},{:8.3f})".format(n,begin,end))
+                      
                         # écrire le contenu dans le tier de destination
                         try:
                               dest.add_interval(begin=begin, end=end, value=sent, check=True)
-                              deb_print("L{} global (begin,end) = ({:8.3f},{:8.3f})".format(n,begin,end))
-                        except: 
-                              print("L{} search fail (begin,end) = (????????,????????)".format(n))
-                              err[inconllFile]+=1
+                        except:
+                              print('Warning: Anti-overlaps')
+                        
                   else:
-                        print("L{} search fail (begin,end) = (????????,????????)".format(n))
-                        err[inconllFile]+=1
-            
-            # préparation à la prochaine phrase
-            sentId += 1
-            tokens = []
+                        # try a global search but with a more strict threshold for distance
+                        [begin,end, cursor_out] = findTimes(tokens,ref, lowerbound=0, upperbound=-1, thld = 0.05)
+                        if cursor_out >= 0 : 
+                              #deb_print("L{} global (begin,end) = ({:8.3f},{:8.3f})".format(n,begin,end))
+                        
+                              # écrire le contenu dans le tier de destination
+                              try:
+                                    dest.add_interval(begin=begin, end=end, value=sent, check=True)
+                                    deb_print("L{} global (begin,end) = ({:8.3f},{:8.3f})".format(n,begin,end))
+                              except: 
+                                    print("L{} search fail".format(n))
+                                    err[inconllFile]+=1
+                        else:
+                              print("L{} search fail".format(n))
+                              err[inconllFile]+=1
+                  
+                  # préparation à la prochaine phrase
+                  sentId += 1
+                  tokens = []
 
-    #path =  "./{}/{}".format(out_rep, outputTgFile)           
-    tg.to_file(outputTg_path, mode='binary', codec='utf-8')
-    print("\n\nDONE.\n\n")
+          #path =  "./{}/{}".format(out_rep, outputTgFile)           
+          tg.to_file(outputTg_path, mode='binary', codec='utf-8')
+          print("\n\nDONE.\n\n")
 
-print("Summaray of processed files: ")
-for n,p in enumerate(conll_tg_pairs_bak): 
-      print('{}:\t{:5s}: {}\n\t{:5s}: {}'.format(n,'conll',p[0],'tg',p[1]))
-      if p[0] in err.keys():
-            num_err = err[p[0]] 
-            if num_err: print('\tnumber of errors: {}'.format(num_err))
-      print('')
-#*****************************
+      print("Summaray of processed file(s): ")
+      show_list_of_file_pair(conll_tg_pairs_bak, err_cnt = err, enc_dict = enc)
+      #*****************************
